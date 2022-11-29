ae51c3e1b8efebdecebb6639b082f32925ebf155
Saif mohamed <94307792+ssaiffff@users.noreply.github.com>
2022-11-29 17:56:28 +0200

    first commit

---
+++ lib/network/remote/dio_helper.dart
@@ -0,0 +1,20 @@
+import 'package:dio/dio.dart';
+
+class DioHelper{
+
+  static Dio ?dio;
+  static init(){
+    dio =Dio(BaseOptions(
+      baseUrl:'https://newsapi.org/' ,
+      receiveDataWhenStatusError: true,
+    ),);
+  }
+  static Future<Response?>  getData({
+    required String url,
+    Map<String,dynamic>? query,
+  })async
+  {
+    return await  dio?.get(url, queryParameters: query,);
+  }
+
+}
